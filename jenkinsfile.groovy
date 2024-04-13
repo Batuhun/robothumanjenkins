9644d74970bdda515199c6494a99ce0f251e9970
Batuhun <51203631+Batuhun@users.noreply.github.com>
2024-04-13 18:56:39 +0200

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,4 +1,4 @@
-pipeline {
+hopipeline {
     agent { 
         node {
             label 'agent1'
@@ -12,7 +12,7 @@ pipeline {
             steps {
                 echo "Building.."
                 sh '''
-                dir
+                echo "dir"
                 '''
             }
         }
