9765819bdbca52d2cabab04e14d94a9f25a722dc
Batuhun <51203631+Batuhun@users.noreply.github.com>
2024-04-14 16:05:42 +0200

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -10,7 +10,7 @@ pipeline {
             steps {
                 echo "Building.."
                 sh '''
-                docker
+                echo "docker"
                 '''
             }
         }
