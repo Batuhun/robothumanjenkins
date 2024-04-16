ee4a75f595708670dca3a4b6dc0e1717c7ba7ce2
Batuhun <barnabas.galacz@gmail.com>
2024-04-16 15:53:19 +0200

    python

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -22,7 +22,7 @@ pipeline {
             steps {
                 echo "Testing.."
                 sh '''
-                echo "doing test stuff.."
+                python3 training.py
                 '''
             }
         }
