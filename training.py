9cc9aba327eb1162f196b42584dfbbdc3187e7e0
Batuhun <barnabas.galacz@gmail.com>
2024-04-16 15:55:17 +0200

    train

--- training.py
+++ training.py
@@ -8,3 +8,10 @@ version = project.version(1)
 dataset = version.download("yolov8")
 
 print(dataset.location+"/data.yaml")
+
+model = YOLO("yolov8l.yaml")  # build a new model from scratch
+model = YOLO("yolov8l.pt")  # load a pretrained model (recommended for training)
+
+# Use the model
+model.train(data=dataset.location+"/data.yaml", epochs=1,imgsz=640)
+metrics = model.val()
