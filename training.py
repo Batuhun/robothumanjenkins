cd0a4b85ac635e74aae57cf7734fb2e254ca9896
Batuhun <barnabas.galacz@gmail.com>
2024-04-17 22:11:37 +0200

    maybe

--- training.py
+++ training.py
@@ -7,18 +7,18 @@ rf = Roboflow(api_key="tcoKTgvDJQbxwi8pIzeg")
 project = rf.workspace("robot-human-detection").project("robot-human-detection")
 version = project.version(1)
 dataset = version.download("yolov8")
-'''
+
 try:
-    with zipfile.ZipFile('/var/jenkins_home/workspace/RobotHumanYOLOv8@2/robot-human-detection-1/roboflow.zip', 'r') as zip_ref:
-        zip_ref.extractall('/var/jenkins_home/workspace/RobotHumanYOLOv8@2/robot-human-detection-1')
+    with zipfile.ZipFile('/var/jenkins_home/workspace/RobotHumanYOLOv8/robot-human-detection-1/roboflow.zip', 'r') as zip_ref:
+        zip_ref.extractall('/var/jenkins_home/workspace/RobotHumanYOLOv8/robot-human-detection-1')
 except:
     print('error unzipping')
-'''
+
 print(os.listdir("/var/jenkins_home/workspace/"))
 print('\n')
 print(os.listdir("/var/jenkins_home/workspace/RobotHumanYOLOv8/"))
 print('\n')
-#os.listdir("/var/jenkins_home/RobotHumanYOLOv8@2/robot-human-detection-1")
+os.listdir("/var/jenkins_home/RobotHumanYOLOv8/robot-human-detection-1")
 
 filename = "/var/jenkins_home/workspace/RobotHumanYOLOv8/robot-human-detection-1/data.yaml"
 with open(filename, "r") as f:
